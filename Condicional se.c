753332142bd51b210e8f6ffc25851fd13c001127
Junior-Santos-15 <128625004+Junior-Santos-15@users.noreply.github.com>
2023-05-17 15:08:30 -0300

    Add files via upload

---
+++ Condicional se.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+
+//Declaração das variáveis
+float nota;
+
+printf("Digite o valor: \n");
+scanf("%f",&nota);
+
+
+system("cls");
+
+//Resolução de resposta
+printf("Valor: %f\n",nota);
+
+if (nota == 10) {
+    printf("O valor igual a 10! \n");
+} else if (nota > 10) {
+    printf("O valor maior que 10! \n");
+} else {
+    printf("O valor menor que 10! \n");
+}
+
+system("pause");
+
+    return 0;
+}
